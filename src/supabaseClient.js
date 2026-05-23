9a5ff7130ade917f6be61b9a5f2116f3dd8ddfea
josecamilocarteh <72116127+josecamilocarteh@users.noreply.github.com>
2026-05-23 02:33:12 -0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Reemplaza estos valores con los tuyos de Supabase
 const SUPABASE_URL = 'https://unqdlgycxhtlehdvpqec.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_pEMcMrbhDL_7cOCpaSxKeA_9t0nLLTJ'
 
